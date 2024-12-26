d774f8bc0734a84b912f1d0522104df913b0edf6
SWITZLIFE <myswitzlife@gmail.com>
2024-12-26 17:43:00 +0000

    Automatic commit before Assistant edits

--- client/src/pages/RewardPage.tsx
+++ client/src/pages/RewardPage.tsx
@@ -6,9 +6,9 @@ import { Link } from "wouter";
 
 export default function RewardPage() {
   return (
-    <div className="min-h-screen p-8 bg-background">
+    <div className="p-8 bg-background">
       <div className="max-w-4xl mx-auto">
-        <div className="flex justify-between items-center mb-8">
+        <div className="flex justify-between items-center mb-8 mt-8">
           <Link href="/">
             <Button variant="ghost" size="sm">
               <ArrowLeft className="h-4 w-4 mr-2" />
