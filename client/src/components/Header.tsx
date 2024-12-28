1edd058d0478d6496ae19993ed0e787e2b22a27d
SWITZLIFE <myswitzlife@gmail.com>
2024-12-28 10:15:34 +0000

    change some styling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,8 +6,8 @@ export function Header() {
     <header className="bg-primary text-primary-foreground fixed w-full z-50">
       <div className="container flex h-10 max-w-screen-2xl items-center justify-between px-6">
         <div className="flex items-center gap-2 font-semibold">
-          Goal Navigator
         </div>
+        
         <UserMenu />
       </div>
     </header>
--- client/src/components/PageHeader.tsx
+++ client/src/components/PageHeader.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export function PageHeader() {
-  return <div className="h-10 flex items-center px-8" />;
+  return <div className="h-10 flex pt-3 px-8">
+    <h1 className="font-bold text-lg ml-auto text-white">Goal Planner</h1>
+  
+  </div>
+    
+    
+    ;
 }
