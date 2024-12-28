9208ad8830171324ebc7fd73fb76b2baebe82534
SWITZLIFE <myswitzlife@gmail.com>
2024-12-28 18:49:20 +0000

    styling header

--- client/src/components/PageHeader.tsx
+++ client/src/components/PageHeader.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function PageHeader() {
-  return <div className="h-10 flex items-center px-8" />;
+  return (
+    <div className="h-10 flex pt-3 px-8 text-white justify-end">
+        <h1 style={{ fontWeight: 'bold', fontSize: '24px' }}>Goal Planner</h1>
+    </div>
+  );
 }
