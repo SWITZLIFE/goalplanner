317cade5ec560b25381b0f78f7ee3a8423b22461
SWITZLIFE <myswitzlife@gmail.com>
2024-12-29 08:26:35 +0000

    Restored to '46a813b1ca2fa8c48b6becf07c387d6389a8bd6d'
    Replit-Restored-To:46a813b1ca2fa8c48b6becf07c387d6389a8bd6d

--- client/src/pages/RewardPage.tsx
+++ client/src/pages/RewardPage.tsx
@@ -1,4 +1,3 @@
-
 import { CoinBalance } from "@/components/rewards/CoinBalance";
 import { RewardStore } from "@/components/rewards/RewardStore";
 import { PageHeader } from "@/components/PageHeader";
@@ -14,18 +13,19 @@ export default function RewardPage() {
       <LeftPanel />
       <div className="flex-1 flex flex-col">
         <PageHeader />
-        <div className="flex-1 m-4 bg-background rounded-[30px] overflow-hidden">
+        <motion.div 
+          className="flex-1 m-4 bg-background rounded-[30px] overflow-hidden"
+          initial={{ x: 50, opacity: 1 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
           <div className="h-full overflow-auto scrollbar-hide py-14 px-14">
-            <motion.div 
-              className="max-w-8xl mx-auto"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.3 }}
-            >
+            <div className="max-w-8xl mx-auto">
+            
               <RewardStore />
-            </motion.div>
+            </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
